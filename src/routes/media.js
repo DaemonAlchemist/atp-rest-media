6c7ff65ff283cad24aff22ec1b028c535e36aa33
Andrea <andrea@wittrock.us>
2017-10-20 22:35:13 -0500

    Refactor to use basic crud controller set

--- src/routes/media.js
+++ src/routes/media.js
@@ -2,52 +2,34 @@
  * Created by Andy on 8/25/2017.
  */
 
-import {basicController, NOT_IMPLEMENTED} from 'atp-rest';
+import {basicController} from 'atp-rest';
 import {createCrudPermissions} from "atp-rest-uac";
 import File from "../model/file";
 import {o} from 'atp-sugar';
 import base64 from 'base-64';
 
+const model = File;
 const permissions = createCrudPermissions('media', 'file');
+const idField = 'fileId';
 
-const restParams = permission => ({
-    model: File,
-    permission,
-    idField: 'fileId',
-    validate: (v, req) => v.isInteger(req.params.fileId, "fileId") //.fileExists(req.params.fileId)
-});
-
-const updateParams = permission => ({
-    model: File,
-    permission,
-    idField: "fileId",
-    validate: v => v, //TODO:  Implement media file edit validations
-});
-
-export default {
+export default o(basicController.entity.crud({model, permissions, idField})).merge({
     get: basicController.entity.collection({
-        model: File,
+        model,
         permission: permissions.view,
         processResults: files => files.map(file => o(file).delete('data').raw)
     }),
-    post: basicController.entity.create({
-        model: File,
-        permission: permissions.create,
-        validate: v => v, //TODO:  Implement file creation validations
-    }),
-    ':fileId': {
-        get: basicController.entity.view(restParams(permissions.view)),
-        put: basicController.entity.replace(updateParams(permissions.edit)),
-        patch: basicController.entity.update(updateParams(permissions.edit)),
-        delete: basicController.entity.delete(restParams(permissions.delete)),
+    [':' + idField]: {
         download: {
             //TODO:  Add support for resizing and cropping
-            get: basicController.entity.view(o(restParams(permissions.view)).merge({
-                    processResults: file => base64.decode(file.data),
-                    raw: true,
-                    contentType: file => file.mime
-                }).raw
-            )
+            get: basicController.entity.view({
+                model,
+                permission: permissions.view,
+                idField,
+                validate: (v, req) => v.isInteger(req.params[idField], idField),
+                processResults: file => base64.decode(file.data),
+                raw: true,
+                contentType: file => file.mime
+            })
         }
     }
-};
+}).raw;
