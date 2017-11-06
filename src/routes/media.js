c46a133c5893a9588ffe3729430c4e2d9bbf1def
Andrea <andrea@wittrock.us>
2017-11-06 00:04:09 -0600

    Implement image resizing

--- src/routes/media.js
+++ src/routes/media.js
@@ -7,6 +7,7 @@ import {createCrudPermissions} from "atp-rest-uac";
 import File from "../model/file";
 import {o} from 'atp-sugar';
 import base64 from 'base-64';
+import sharp from 'sharp';
 
 const model = File;
 const permissions = createCrudPermissions('media', 'file');
@@ -26,7 +27,14 @@ export default o(basicController.entity.crud({model, permissions, idField})).mer
                 permission: permissions.view,
                 idField,
                 validate: (v, req) => v.isInteger(req.params[idField], idField),
-                processResults: file => base64.decode(file.data),
+                processResults: (file, req) => req.query.width || req.query.height
+                    ? sharp(Buffer.from(file.data, 'base64'))
+                        .resize(+req.query.width || null, +req.query.height || null)
+                        .background({r: 0, g: 0, b: 0, alpha: 0})
+                        .embed()
+                        .toFormat(sharp.format.png)
+                        .toBuffer()
+                    : Buffer.from(file.data, 'base64'),
                 raw: true,
                 contentType: file => file.mime
             })
