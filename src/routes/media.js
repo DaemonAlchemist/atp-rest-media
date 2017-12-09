5295680bf502c15d6d51a7e613f5c7f49fa06c27
Andrew Wittrock <andy@wittrock.us>
2017-12-09 15:37:49 -0600

    Refactor to use S3 for storage

--- src/routes/media.js
+++ src/routes/media.js
@@ -5,10 +5,6 @@
 import {basicController} from 'atp-rest';
 import {createCrudPermissions} from "atp-rest-uac";
 import File from "../model/file";
-import {o} from 'atp-sugar';
-import base64 from 'base-64';
-import sharp from 'sharp';
-import {remove, map} from 'atp-pointfree';
 import config from 'atp-config';
 import AWS from 'aws-sdk';
 
@@ -16,14 +12,15 @@ const model = File;
 const permissions = createCrudPermissions('media', 'file');
 const idField = 'fileId';
 
-export default o(basicController.entity.crud({
+export default basicController.entity.crud({
     model, permissions, idField,
     preInsert: file => new Promise((resolve, reject) => {
         const awsConfig = config.get('media.aws');
         AWS.config.update({region: awsConfig.region})
         const s3 = new AWS.S3({params: {Bucket: awsConfig.bucket}});
+        const fileNameFull = file.fileName + "." + file.fileExtension;
         s3.upload({
-            Key: awsConfig.folder + "/" + file.name,
+            Key: fileNameFull,
             Body: Buffer.from(file.data, 'base64'),
             ACL: 'public-read',
         }, (err, data) => {
@@ -31,30 +28,8 @@ export default o(basicController.entity.crud({
                 reject(err.message)
             } else {
                 delete file.data;
-                file.url = `https://${awsConfig.staticHost}/${awsConfig.bucket}/${awsConfig.folder}/${file.name}`;
                 resolve(file);
             }
         });
     })
-})).merge({
-    [':' + idField]: {
-        download: {
-            get: basicController.entity.view({
-                model,
-                permission: permissions.view,
-                idField,
-                validate: (v, req) => v.isInteger(req.params[idField], idField),
-                processResults: (file, req) => req.query.width || req.query.height
-                    ? sharp(Buffer.from(file.data, 'base64'))
-                        .resize(+req.query.width || null, +req.query.height || null)
-                        .background({r: 0, g: 0, b: 0, alpha: 0})
-                        .embed()
-                        .toFormat(sharp.format.png)
-                        .toBuffer()
-                    : Buffer.from(file.data, 'base64'),
-                raw: true,
-                contentType: file => file.mime
-            })
-        }
-    }
-}).raw;
+});
