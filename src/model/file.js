87b297617e8d235c45a8ba3e78d5e620f676aeda
Andrew Wittrock <andy@wittrock.us>
2018-08-20 16:39:33 -0500

    Add support for tag filters

--- src/model/file.js
+++ src/model/file.js
@@ -3,11 +3,26 @@
  */
 
 import {Entity} from 'atp-active-record';
-import config from 'atp-config';
+import {remove} from "atp-pointfree";
 
 export default class File extends Entity
 {
     constructor() {
         super('media', 'atpmedia_media');
+    }
+
+    filter(filters) {
+        const tags = [].concat(filters.tag || []);
+        filters = remove("tag")(filters);
+        console.log("Media filters: ");
+        console.log(filters);
+        console.log("Media tags");
+        console.log(tags);
+
+        if(tags.length > 0) {
+            this.where(`id in (select entityId from atptag_entity_tag_compiled where entityType="mediaFile" and tag in (${tags.map(tag => `"${tag}"`).join(",")}))`);
+        }
+
+        return super.filter(filters);
     }
 }
